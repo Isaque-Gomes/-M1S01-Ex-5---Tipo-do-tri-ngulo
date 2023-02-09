c09aa8b7f090ca89641fa6d0ac5c88ba89481763
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 09:04:59 -0300

    [M1S01] Ex 5 - Tipo do triângulo
    [Estrutura de Decisão]
    Faça um programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo, primeiro passo será verificar se os lados formam um triângulo. Para isso valide: 
     Se a soma de quaisquer dos dois lados é maior que o terceiro:
    ●	Lado1 + Lado2 > Lado3 e;
    ●	Lado3 + Lado2 > Lado1 e;
    ●	Lado3 + Lado1 > Lado2 e;
    Caso nenhuma das opções seja verdadeira, seu programa deverá informar que as medidas não formam um triângulo e encerrar.
    
    Caso formem um triângulo, informe qual o tipo dele: equilátero, isósceles ou escaleno. Dicas:
    ●	Triângulo Equilátero: três lados iguais;
    ○	Lado1 = Lado2 = lado3
    ●	Triângulo Isósceles: quaisquer dois lados iguais;
    ○	Lado1 = Lado2 ou;
    ○	Lado2 = Lado3 ou;
    ○	Lado3 = Lado1 ou;
    ●	Triângulo Escaleno: três lados diferentes;
    ○	Lado1 != Lado2 E;
    ○	Lado2 != Lado3 E;
    ○	Lado3 != Lado1 E;

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// 3 lados de um triângulo
+//validar os lados e depois dizer o tipo do triângulo
+float lado1;
+float lado2;
+float lado3;
+
+Console.WriteLine("Olá, vamos validar um triângulo. Para isso, a seguir, digite o valor dos três lados.");
+Console.WriteLine("Digite o valor do lado 1:");
+lado1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite o valor do lado 2:");
+lado2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Por fim, digite o valor do lado 3:");
+lado3 = float.Parse(Console.ReadLine());
+if((lado1 + lado2) > lado3 && (lado3 + lado2) > lado1 && (lado3 + lado1) > lado2){
+    Console.WriteLine("Não formam um triângulo");
+    return;
+}
+Console.WriteLine("Os lados formam um triângulo!");
+if(lado1 == lado2 && lado2 == lado3 && lado1 == lado3){
+    Console.WriteLine("As medidas formam um triângulo equilátero (três lados iguais).");
+}
+else if(lado1 == lado2 || lado2 == lado3 || lado1 == lado3){
+    Console.WriteLine("As medidas formam um triângulo isósceles (quaisquer dois lados iguais).");
+}
+else if(lado1 != lado2 && lado2 != lado3 && lado1 != lado3){
+    Console.WriteLine("As medidas formam um triângulo escaleno (três lados diferentes).");
+}
+
